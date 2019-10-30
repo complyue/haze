f73fc06e2ae56aab4afe041ea3f6c125715d920a
ComplYue <compl.yue@icloud.com>
2019-10-30 22:01:08 +0800

    plot api wip

--- haze/src/Haze.hs
+++ haze/src/Haze.hs
@@ -1,16 +1,21 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ExtendedDefaultRules #-}
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE QuasiQuotes #-}
 
 module Haze where
 
 import           UIO
 
+import qualified RIO.Vector                    as V
+
 import qualified Data.Map                      as Map
 
 import           NeatInterpolation
 
+import qualified Data.Aeson                    as A
 import           Data.Aeson.QQ                  ( aesonQQ )
 
 import           PlotCtx
@@ -18,46 +23,84 @@ import           PlotCtx
 default (Text, Int)
 
 
-withPlotGroup :: (PlotGroup -> UIO ()) -> UIO ()
-withPlotGroup grpPlot = do
-    axes' <- newIORef Map.empty
-    let nla = LinkedAxis { axisName = "narr-timeline" }
-        ntl = NarrTimeline { axis = nla }
-        pg =
-            PlotGroup { plotGrpId = "123" -- TODO use a global sequence or sth
-                                         , narrTimeline = ntl, axes = axes' }
+openPlotWindow :: PlotGroup -> WindowId -> UIO PlotWindow
+openPlotWindow pg winId = do
+    dsiw <- newDeque
+    pfs  <- newIORef []
+    let pw = PlotWindow { plotWinId       = winId
+                        , plotGroup       = pg
+                        , dsInWindow      = dsiw
+                        , figuresInWindow = pfs
+                        }
+
+    modifyIORef' (windowsInGroup pg) $ (:) pw
+
+    return pw
+
+
+putDataSource :: PlotWindow -> ColumnDataSource -> UIO DataSourceRef
+putDataSource pw cds = do
+    let !dsiw = dsInWindow pw
+    dsr <- getDequeSize dsiw
+    pushBackDeque dsiw cds
+    return dsr
+
+
+addPlotFigure :: PlotWindow -> (Map ArgName A.Value) -> UIO PlotFigure
+addPlotFigure pw figArgs = do
+    figureArgs' <- newIORef figArgs
+    figureOps'  <- newIORef []
+    linkedAxes' <- newIORef Map.empty
+    let pf = PlotFigure { plotWindow = pw
+                        , figureArgs = figureArgs'
+                        , figureOps  = figureOps'
+                        , linkedAxes = linkedAxes'
+                        }
+
+    modifyIORef' (figureArgs pf) $ flip Map.alter "tools" $ \case
+        -- with tools if not explicitly specified, default to this list
+        Nothing -> Just $ A.Array $ V.fromList
+            [ "crosshair"
+            , "pan"
+            , "xwheel_zoom"
+            , "ywheel_zoom"
+            , "box_zoom"
+            , "hover"
+            , "undo"
+            , "redo"
+            , "reset"
+            ]
+        Just tools -> Just tools
+
+    modifyIORef' (figuresInWindow pw) $ (:) pf
+
+    return pf
+
+
+onFigure :: PlotFigure -> FigureOp -> UIO ()
+onFigure pf op = modifyIORef' (figureOps pf) $ (:) op
+
+
+linkAxis :: PlotFigure -> AxisField -> AxisRef -> UIO ()
+linkAxis pf field axis = modifyIORef' (linkedAxes pf) $ Map.insert field axis
+
+
+defineAxis :: PlotGroup -> UIO AxisRef
+defineAxis pg = atomicModifyIORef' (numOfLinkedAxes pg) $ \i -> (i + 1, i)
+
+
+showPlot :: GroupId -> (PlotGroup -> UIO ()) -> UIO ()
+showPlot grpId grpPlot = do
+    nla <- newIORef 0
+    pws <- newIORef []
+    let pg = PlotGroup { plotGrpId       = grpId
+                       , numOfLinkedAxes = nla
+                       , windowsInGroup  = pws
+                       }
 
     grpPlot pg
 
-
-withPlotWindow :: PlotGroup -> Text -> (PlotWindow -> UIO ()) -> UIO ()
-withPlotWindow pg wRef winPlot = do
-    let pw = PlotWindow { webWindowRef = wRef, plotGroup = pg }
-
-    winPlot pw
-
-
-withPlotFigure :: PlotWindow -> (PlotFigure -> UIO ()) -> UIO ()
-withPlotFigure pw figPlot = do
-    figureTitle' <- liftIO $ newIORef "Haze Plot"
-    [figureTools', figureSetup', glyphSetup', legendSetup'] <- liftIO
-        $ mapM newIORef [["zoom", "pan", "crosshair"], [], [], []]
-    let pf = PlotFigure { plotWindow  = pw
-                        , figureTitle = figureTitle'
-                        , figureTools = figureTools'
-                        , figureSetup = figureSetup'
-                        , glyphSetup  = glyphSetup'
-                        , legendSetup = legendSetup'
-                        }
-
-    figPlot pf
-
-drawGlyph :: PlotFigure -> Text -> UIO ()
-drawGlyph pf glType = do
-
-    liftIO $ modifyIORef (glyphSetup pf) $ (:) [text|
-${glType}()
-|]
+    -- TODO send json cmds out
 
 
 showHazePage :: Text -> UIO ()
--- haze/src/PlotCtx.hs
+++ haze/src/PlotCtx.hs
@@ -6,46 +6,59 @@
 module PlotCtx where
 
 import           RIO
+import qualified RIO.Vector.Storable           as VS
+
+import qualified Data.Aeson                    as A
 
 
--- |
-data LinkedAxis = LinkedAxis {
-    axisName :: Text
-
-}
+type AxisRef = Int
 
 
--- |
-data NarrTimeline = NarrTimeline {
-    axis :: LinkedAxis
-}
-
-
+type GroupId = Text
 -- | a group has a single narrative timeline, and defines
 -- a set of named axes to be linked in pan/zoom
 data PlotGroup = PlotGroup {
-    plotGrpId :: Text
-    , narrTimeline :: NarrTimeline
-    , axes :: IORef (Map Text LinkedAxis)
+    plotGrpId :: GroupId
+    , numOfLinkedAxes :: IORef Int
+    , windowsInGroup :: IORef [PlotWindow]
 }
 
 
-data PlotWindow = PlotWindow {
-    -- | `target` wrt openWindow in js, where `_blank` will
-    -- always open new window
-    webWindowRef :: Text
-    -- | id of group to have all x-axis/y-axis sync'ed
+type ColumnName = Text
+type ColumnData = VS.MVector (PrimState IO) Double
+-- todo other types of array element to support ?
+-- note js within any browser inherently has no support of int64, 
+-- int32/int16/int8 worth to be added beyond float64 ?
+type ColumnDataSource = Map ColumnName ColumnData
+
+
+type WindowId = Text
+-- | a window mapped to a browser window (tab actually nowadays)
+data PlotWindow =  PlotWindow {
+    plotWinId :: WindowId
     , plotGroup :: PlotGroup
+    , dsInWindow :: BDeque (PrimState IO) ColumnDataSource
+    , figuresInWindow :: IORef [PlotFigure]
 }
 
 
+type DataSourceRef = Int
+type ArgName = Text
+data ArgValue = DataField ColumnName | DataValue A.Value  | LiteralValue A.Value
+
+type MethodName = Text
+type AttrName = Text
+data FigureOp = FigureCall MethodName (Maybe DataSourceRef) (Map ArgName ArgValue)
+    | FigureUpdate [AttrName] A.Value
+    | FigureMod MethodName A.Value
+
+
+type AxisField = Text
+-- | a figure with glyphs and layout elements
 data PlotFigure = PlotFigure {
     plotWindow :: PlotWindow
-    , figureTitle :: IORef Text
-    , figureTools :: IORef [Text]
-    , figureSetup :: IORef [Text]
-    , glyphSetup  :: IORef [Text]
-    , legendSetup :: IORef [Text]
+    , figureArgs :: IORef (Map ArgName A.Value)
+    , figureOps  :: IORef [FigureOp]
+    , linkedAxes :: IORef (Map AxisField AxisRef)
 }
-
 
