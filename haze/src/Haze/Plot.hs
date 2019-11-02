b109daf54f2f381ca8cd686df7ce40f0b870b95a
Compl Yue <compl.yue@icloud.com>
2019-11-02 21:47:05 +0800

    wip

--- haze/src/Haze/Plot.hs
+++ haze/src/Haze/Plot.hs
@@ -37,6 +37,17 @@ import qualified Network.WebSockets            as WS
 import           Haze.Types
 
 
+totalDataSize :: PlotGroup -> UIO Double
+totalDataSize pg = do
+    pws <- readIORef $ windowsInGroup pg
+    tdl <- foldM cumWin (0 :: Int64) pws
+    return $ fromIntegral tdl * fromIntegral (sizeOf (0 :: Double))
+  where
+    cumWin ds pw = foldlDeque cumCDS ds (dsInWindow pw)
+    cumCDS ds cds = foldM cumCol ds $ Map.elems cds
+    cumCol ds cd = return $ ds + (fromIntegral $ VM.length cd)
+
+
 uiPlot :: GroupId -> (PlotGroup -> UIO ()) -> UIO ()
 uiPlot grpId plotProcedure =
     (ask >>= \uio ->
@@ -66,58 +77,81 @@ uiPlot grpId plotProcedure =
                            , numOfLinkedAxes = nla_
                            , windowsInGroup  = pws_
                            }
-
-        -- Haskell code use the DSL to do plot setup
+        -- realize the procedure which uses the DSL to do plot setup
         plotProcedure pg
-
-        -- all windows defined
-        pws <- readIORef (windowsInGroup pg)
-
         -- report total data size to UI, get the user some intuition
         -- for how long he/she should expect before all rendered
-        tds <- totalDataSize pws
+        tds <- totalDataSize pg
         uiMsg
             $  "total plot data size: "
             <> (T.pack $ printf "%0.1f" (tds / 1024 / 1024))
             <> " MB"
+        -- interpret the stated resulted from DSL, by sending json
+        -- cmds and binary column data to UI
+        outputPlot wsc pg
 
-        -- send json cmds and binary column data to UI
-        outputPlot wsc pg pws
+
+outputPlot :: WS.Connection -> PlotGroup -> UIO ()
+outputPlot wsc pg = do
+    -- nAxes <- readIORef $ numOfLinkedAxes pg
+    pws <- readIORef (windowsInGroup pg)
+    for_ pws $ outputWin wsc
+
+--     let plotCode = [text|
+-- console.log('here it is!')
+-- |]
+--     wsSendText
+--         wsc
+--         [aesonQQ|{
+-- "type": "call"
+-- , "name": "plotGroup"
+-- , "args": [#{plotCode}]
+-- }|]
 
 
-outputPlot :: WS.Connection -> PlotGroup -> [PlotWindow] -> UIO ()
-outputPlot wsc pg pws = do
-    forM_ pws sendDiW
-
-    let plotCode = [text|
-console.log('here it is!')
-|]
+outputWin :: WS.Connection -> PlotWindow -> UIO ()
+outputWin wsc pw = do
+    cnl <- foldrDeque processCDSQ [] dsiw
+    pfs <- readIORef $ figuresInWindow pw
+    pcl <- foldM outputFigure [] pfs
+    let !plotCode =
+            T.unlines
+                $  ["async function(pgid, pwid, cdsa){\n\n"]
+                ++ pcl
+                ++ ["\n}\n"]
     wsSendText
         wsc
         [aesonQQ|{
 "type": "call"
-, "name": "plot"
-, "args": [#{plotCode}]
-}|]
-  where
-    forDeque q f = foldlDeque (const f) () q
-    sendDiW pw = forDeque (dsInWindow pw) $ \cds -> do
-        for_ (Map.elems cds) $ \cd -> wsSendData wsc cd
-        -- 
-        wsSendText
-            wsc
-            [aesonQQ|{
-"type": "call"
 , "name": "plotWin"
-, "args": []
+, "args": [#{pgid}, #{pwid}, #{cnl}, #{plotCode}]
 }|]
 
-
-totalDataSize :: [PlotWindow] -> UIO Double
-totalDataSize pws = do
-    tdl <- foldM cumWin (0 :: Int64) pws
-    return $ fromIntegral tdl * fromIntegral (sizeOf (0 :: Double))
   where
-    cumWin ds pw = foldlDeque cumCDS ds (dsInWindow pw)
-    cumCDS ds cds = foldM cumCol ds $ Map.elems cds
-    cumCol ds cd = return $ ds + (fromIntegral $ VM.length cd)
+    !dsiw = dsInWindow pw
+    !pwid = plotWinId pw
+    !pg   = plotGroup pw
+    !pgid = plotGrpId pg
+
+    processCDSQ :: ColumnDataSource -> [[Text]] -> UIO [[Text]]
+    -- column names are listed in cds/col order,
+    -- each column has its binary data sent as one ws packet,
+    -- but columns are sent in revered cds/col order, so at js site
+    -- each column is poped from the stack of received packets.
+    processCDSQ cds cnl = do
+        for_ (reverse $ Map.elems cds) $ \cd -> wsSendData wsc cd
+        return $ Map.keys cds : cnl
+
+
+outputFigure :: [Text] -> PlotFigure -> UIO [Text]
+outputFigure pcl pf = do
+    figArgs <- readIORef $ figureArgs pf
+    figOps  <- readIORef $ figureOps pf
+    lAxes   <- readIORef $ linkedAxes pf
+
+    let pcl' = "xx" : pcl
+
+    return $ pcl'
+    where pw = plotWindow pf
+
+
