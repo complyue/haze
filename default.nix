664d559093a38c68ab7f12511438a077e0fff631
ComplYue <compl.yue@icloud.com>
2019-11-20 18:52:47 +0800

    use local hadui

--- default.nix
+++ default.nix
@@ -1,13 +1,13 @@
 { overlays ? [ ], ... }@args:
 import (
 # to use a version of Hadui from github
-  builtins.fetchTarball {
-    url = "https://github.com/complyue/hadui/archive/0.1.0.2.tar.gz";
-    sha256 = "0lz2i7ycaj7pj82bjjkm5cchq3ds23v9i2pkc3hzq16p7hvb67lz";
-  }
+#   builtins.fetchTarball {
+#     url = "https://github.com/complyue/hadui/archive/0.1.0.2.tar.gz";
+#     sha256 = "0lz2i7ycaj7pj82bjjkm5cchq3ds23v9i2pkc3hzq16p7hvb67lz";
+#   }
 
   # to use the version of Hadui checked out locally
-  #../hadui
+  ../hadui
 ) (args // {
   overlays = [
     (self: super:
